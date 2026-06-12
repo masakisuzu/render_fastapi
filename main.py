8eb3d8d721c62af1b552d98b1087709f62d17256
masakisuzu <kmym0417@icloud.com>
2026-06-12 12:09:12 +0900

    kadai1.0

--- main.py
+++ main.py
@@ -14,19 +14,37 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/omikuji")
-def omikuji():
-    omikuji_list = [
-        "大吉",
-        "中吉",
-        "小吉",
-        "吉",
-        "半吉",
-        "末吉",
-        "末小吉",
-        "凶",
-        "小凶",
-        "大凶"
-    ]
+# @app.get("/omikuji")
+# def omikuji():
+#     omikuji_list = [
+#         "大吉",
+#         "中吉",
+#         "小吉",
+#         "吉",
+#         "半吉",
+#         "末吉",
+#         "末小吉",
+#         "凶",
+#         "小凶",
+#         "大凶"
+#     ]
+#     return omikuji_list[random.randrange(10)]
 
-    return omikuji_list[random.randrange(10)]
+
+from fastapi.responses import HTMLResponse
+
+### コードいろいろ... ###
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
